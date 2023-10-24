8858f676be675c8bec6cc046580ab0d5b6867bff
Dhruv <38424726+fullmeteor172@users.noreply.github.com>
2023-10-24 15:15:38 +0530

    Revert "Major front end rework, moving to svelte."

--- website/src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import HeroBanner from '../components/Banner.svelte';
-    import ProfilesSection from '../components/ProfilesSection.svelte';
-</script>
-<HeroBanner/>
-<ProfilesSection/>
